bd539e7bffb5fea88b8b2e0ed3cda76f0453df53
Erik Contena <contenaerik@gmail.com>
2020-08-06 18:57:29 +0800

    materi

---
+++ application/views/home/materi.php
@@ -0,0 +1,64 @@
+<div class="container" style="margin-top:50px">
+  <div class="row ">
+    <div class="col-sm-3"style="position:fixed ">
+      <h3 style=" margin-top:50px">Materi Praktik Shalat</h3>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Pengertian Sholat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#pengertian">Hukum Sholat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#hukum">Sholat Lima Waktu</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#syarat">Syarat-syarat Sholat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#rukun">Rukun Sholat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#sunah">Sunah-Sunah Sholat</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#tatacara">Tata Cara Sholat Tuntunan Rasulullah</a>
+        </li>
+        
+      </ul>
+      <hr class="d-sm-none">
+    </div>
+    <div class="col-sm-8  offset-4"style="margin-top:50px">
+      <h2 id="pengertian">Pengertian Sholat</h2>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatibus ipsum animi sapiente debitis corporis at excepturi ipsa deserunt! Excepturi numquam repellat eveniet perspiciatis dignissimos odit quasi qui itaque architecto. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <br>
+      <h2 id="hukum">Hukum Sholat</h2>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo dolores suscipit repellat quis iusto ab necessitatibus aliquam? Exercitationem consequatur error amet ratione, nobis culpa officiis quae eaque beatae? Ex. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <h2 id="sholat">Sholat 5 Waktu</h2>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo dolores suscipit repellat quis iusto ab necessitatibus aliquam? Exercitationem consequatur error amet ratione, nobis culpa officiis quae eaque beatae? Ex. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <h2 id="syarat">Syarat-Syarat Sholat</h2>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo dolores suscipit repellat quis iusto ab necessitatibus aliquam? Exercitationem consequatur error amet ratione, nobis culpa officiis quae eaque beatae? Ex. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <h2 id="rukun">Rukun Sholat</h2>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo dolores suscipit repellat quis iusto ab necessitatibus aliquam? Exercitationem consequatur error amet ratione, nobis culpa officiis quae eaque beatae? Ex. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <h2 id="sunah">Sunnah-Sunnah Sholat</h2>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo dolores suscipit repellat quis iusto ab necessitatibus aliquam? Exercitationem consequatur error amet ratione, nobis culpa officiis quae eaque beatae? Ex. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <h2 id="tatacara">Tata Cara Sholat</h2>
+      <div class="fakeimg">Fake Image</div>
+      <p>Some text..</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo dolores suscipit repellat quis iusto ab necessitatibus aliquam? Exercitationem consequatur error amet ratione, nobis culpa officiis quae eaque beatae? Ex. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+    
+    </div>
+  </div>
+</div>
