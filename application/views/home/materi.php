516efc2acbd75e91cabe7fbd1b21efad51564d29
Erik Contena <contenaerik@gmail.com>
2020-08-07 17:16:46 +0800

    fix materi

--- application/views/home/materi.php
+++ application/views/home/materi.php
@@ -22,43 +22,122 @@
           <a class="nav-link" href="#sunah">Sunah-Sunah Sholat</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#tatacara">Tata Cara Sholat Tuntunan Rasulullah</a>
+          <a class="nav-link" href="#tatacara">Gerakan Sholat</a>
         </li>
         
       </ul>
       <hr class="d-sm-none">
     </div>
-    <div class="col-sm-8  offset-4"style="margin-top:50px">
+    <div class="col-sm-9  offset-4"style="margin-top:50px">
       <h2 id="pengertian">Pengertian Sholat</h2>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptatibus ipsum animi sapiente debitis corporis at excepturi ipsa deserunt! Excepturi numquam repellat eveniet perspiciatis dignissimos odit quasi qui itaque architecto. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <h2></h2>
+      <p>Sholat menurut ahli fiqih adalah suatu tindak ibadah disertai bacaan doa-doa yang diawali dengan takbir dan diakhiri dengan salam sesuai dengan syarat-syarat dan rukun-rukunnya. Makna yang sebenarnya adalah menghadapkan jiwa (hati dan pikiran) kepada Allah SWT untuk menumbuhkan rasa takut dan rasa bersalah diri kepada-Nya, serta mengakui keagungan dan kesempurnaan-Nya.
+Setiap orang yang mengaku dirinya islam, wajib mendirikan sholat. Firman Allah SWT dalam Surat Al Baqoroh ayat 4:” dirikanlah sholat, tunaikanlah zakat, dan rukuklah bersama orang-orang yang rukuk (berjamaah)”. Sedangkan manfaatnya bagi orang-orang yang beriman, diterangkan dalam Surat Al Ankabut ayat 45: ”Sesungguhnya shalat itu mencegah dari (perbuatan-perbuatan) keji dan munkar”.
+</p>
       <br>
       <h2 id="hukum">Hukum Sholat</h2>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo dolores suscipit repellat quis iusto ab necessitatibus aliquam? Exercitationem consequatur error amet ratione, nobis culpa officiis quae eaque beatae? Ex. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <p>Hukum sholat dapat dikategorikan sebagai berikut:<br>
+1.	Fardhu (Sholat Wajib), Sholat fardhu ialah sholat yang diwajibkan untuk mengerjakannya. Sholat fardhu terbagi lagi menjadi dua, yaitu:
+    <br> &nbsp;&nbsp; a.	Fardhu „Ain: diwajibkan kepada mukallaf langsung berkaitan dengan dirinya dan tidak boleh ditinggalkan ataupun dilaksanakan oleh orang lain, seperti sholat wajib lima waktu.
+    <br> &nbsp;&nbsp;b.	Fardhu Kifayah: diwajibkan kepada pihak mukallaf tidak langsung berkaitan dengan dirinya. Kewajiban itu menjadi sunnah setelah ada sebagian orang yang mengerjakannya namun akan berdosa apabila tidak ada yang mengerjakan, seperti sholat jenazah.
+    <br>2.	Sholat sunnah ialah sholat yang dianjurkan atau disunnahkan akan tetapi tidak diwajibkan. Sholat sunnah terbagi menjadi dua, yaitu:
+    <br>&nbsp;&nbsp;  a.	Sunnah Mu‟akad adalah shalat sunnah yang dianjurkan dengan penekanan yang kuat (hampir seperti wajib), seperti sholat dua hari raya (ied), sholat sunnah witir dan sholat sunnah thowaf.
+    <br>&nbsp;&nbsp;  b.	Sunnah Ghoiru Mu‟akad adalah sholat sunnah yang dianjurkan tanpa penekanan yang kuat, seperti sholat sunnah rawatib dan sholat sunnah yang sifatnya insidental (tergantung waktu dan keadaan, seperti sholat gerhana).
+</p>
       <h2 id="sholat">Sholat 5 Waktu</h2>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo dolores suscipit repellat quis iusto ab necessitatibus aliquam? Exercitationem consequatur error amet ratione, nobis culpa officiis quae eaque beatae? Ex. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <p>Sholat lima waktu memiliki hukum fardhu „ain dan terdiri atas:
+      <br>&nbsp;&nbsp;1.	Subuh, terdiri dari 2 rakaat. Waktu subuh diawali dari munculnya fajar shaddiq, yakni cahaya putih yang melintang di ufuk timur. Waktu subuh berakhir ketika terbitnya matahari.
+      <br>&nbsp;&nbsp;2.	Dzuhur, terdiri dari 4 rakaat. Waktu dzuhur diawali jika matahari telah tergelincir (condong ke arah barat) dan berakhir ketika masuk waktu ashar.
+      <br>&nbsp;&nbsp;3.	Ashar, terdiri dari 4 rakaat. Waktu ashar diawali jika bayang-bayang benda melebihi panjang benda itu sendiri. Khusus untuk Mazhab Hanafi, waktu ashar dimulai jika panjang bayangan benda dua kali panjang benda itu sendiri. Waktu ashar berakhir dengan terbenamnya matahari.
+      <br>&nbsp;&nbsp;4.	Maghrib, terdiri dari 3 rakaat. Waktu maghrib diawali dengan terbenamnya matahari dan berakhir dengan masuknya waktu isya‟.
+      <br>&nbsp;&nbsp;5.	Isya’, terdiri dari 4 rakaat. Waktu Isya diawali dengan hilangnya cahaya merah (syafaq) di langit barat dan berakhir hingga terbitnya fajar shadiq keesokan harinya.
+</p>
       <h2 id="syarat">Syarat-Syarat Sholat</h2>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo dolores suscipit repellat quis iusto ab necessitatibus aliquam? Exercitationem consequatur error amet ratione, nobis culpa officiis quae eaque beatae? Ex. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <p>
+      &nbsp;&nbsp;1.	Muslim,
+      <br>&nbsp;&nbsp;2.	Berakal,
+<br>&nbsp;&nbsp;3.	Tamyiz (dapat membedakan antara yang baik dan yang buruk),
+<br>&nbsp;&nbsp;4.	Menghilangkan hadats besar maupun kecil,
+<br>&nbsp;&nbsp;5.	Menghilangkan najis di badan, pakaian dan tempat,
+<br>&nbsp;&nbsp;6.	Menutup aurat,
+<br>&nbsp;&nbsp;7.	Masuknya waktu sholat,
+<br>&nbsp;&nbsp;8.	Menghadap kiblat,
+ 
+</p>
       <h2 id="rukun">Rukun Sholat</h2>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo dolores suscipit repellat quis iusto ab necessitatibus aliquam? Exercitationem consequatur error amet ratione, nobis culpa officiis quae eaque beatae? Ex. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+      <p>
+      &nbsp;&nbsp;1.	Niat
+      <br>&nbsp;&nbsp;Niat berarti menyengaja untuk sholat, menghambakan diri kepada Allah SWT semata, serta menguat- kannya dalam hati. Rasulullah SAW bersabda: “Semua amal tergantung pada niatnya dan setiap orang akan mendapat (balasan) sesuai dengan niatnya”. (HR. Bukhori dan lain-lain. Baca Al Irwa‟, hadits no.22).
+      <br>&nbsp;&nbsp; 2.	Takbiratul ihram
+      <br>&nbsp;&nbsp; Takbiratul ihram yaitu takbir yang pertama ketika diucapkan oleh orang yang mengerjakan sholat se- bagai tanda mulai mengerjakan sholat dengan lafadz (ucapan) Allahu Akbar (Allah Maha Besar).
+      <br>&nbsp;&nbsp; 3.	Berdiri bagi yang mampu
+      <br>&nbsp;&nbsp; 4.	Membaca Surah Al Fatihah
+      <br>&nbsp;&nbsp; 5.	Ruku‟ dengan thu‟maninah (tenang dan berhenti sejenak)
+      <br>&nbsp;&nbsp; 6.	I‟tidal dengan thu‟maninah
+      <br>&nbsp;&nbsp; 7.	Sujud dua kali dengan thu‟maninah
+      <br>&nbsp;&nbsp; 8.	Duduk di antara dua sujud dengan thu‟maninah
+      <br>&nbsp;&nbsp; 9.	Duduk dengan thu‟maninah,
+      <br>&nbsp;&nbsp; 10.	Membaca tasyahud akhir.
+      <br>&nbsp;&nbsp; 11.	Membaca sholawat Nabi.
+      <br>&nbsp;&nbsp; 12.	Mengucapkan salam
+      <br>&nbsp;&nbsp; 13.	Tertib (melakukan rukun secara berurutan). Sabda Rasulullah: “Shalatlah kalian sebagaimana kalian melihatku sholat” (HR. Bukhori).
+</p>
       <h2 id="sunah">Sunnah-Sunnah Sholat</h2>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo dolores suscipit repellat quis iusto ab necessitatibus aliquam? Exercitationem consequatur error amet ratione, nobis culpa officiis quae eaque beatae? Ex. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-      <h2 id="tatacara">Tata Cara Sholat</h2>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quo dolores suscipit repellat quis iusto ab necessitatibus aliquam? Exercitationem consequatur error amet ratione, nobis culpa officiis quae eaque beatae? Ex. Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-    
+
+      <p>
+      Sholat mempunyai beberapa sunnah yang dianjurkan untuk kita kerjakan sehingga menambah pahala kita menjadi banyak, diantaranya:
+<br>&nbsp;&nbsp;1.	Mengangkat kedua tangan sehingga sama tinggi ujung jari dengan telinga dan telapak tangan setinggi bahu serta keduanya menghadap kiblat pada keadaan sebagai berikut:
+<br>&nbsp;&nbsp;a.	Ketika takbiratul ihram
+<br>&nbsp;&nbsp;b.	Ketika ruku‟ ketika bangkit dari ruku‟
+<br>&nbsp;&nbsp;c.	Ketika berdiri setelah rakaat kedua ke rakaat ketiga
+<br>&nbsp;&nbsp;Hal ini berdasarkan hadits dari Ibnu Umar r.a: “Bahwasanya Nabi SholAllahu alaihi wasallam apa- bila beliau melaksanakan sholat, beliau mengangkat kedua tangannya sampai sejajar dengan kedua bahu beliau, kemudian membaca takbir. Apabila beliau ingin ruku‟ beliau pun mengangkat kedua tan- gannya seperti itu dan begitu pula kalau beliau bangkit dari ruku‟ beliaupun mengangkat kedua tan- gannya seperti itu dan begitu pula beliau bangkit dari sujud.” (Muttafaq‟alaih)
+<br>&nbsp;&nbsp;2.	Meletakkan tangan kanan di atas tangan kiri di atas dada atau di bawah dada dan di atas pusar. Hal ini berdasarkan  perkataan Sahl bin Sa‟d r.a ;”Orang-orang (di masa Nabi ShollAllahu alaihi wasallam) disuruh untuk meletakkan tangan kanan di atas tangan kiri dalam sholat”. (HR. Bukhori secara mau- quf. Al Hafidz ibnu Hajar berkata:”Riwayat ini dihukumi marfu”).
+<br>&nbsp;&nbsp;3.	Senantiasa melihat ke tempat sujud kecuali pada waktu membaca “Asyhadu alla ilaaha illAllah” (pada saat duduk membaca tasyahud) maka pada waktu itu melihat ke telunjuknya.
+<br>&nbsp;&nbsp;4.	Membaca doa iftitah sesudah takbiratul ihram sebelum membaca surah Al Fatihah.
+<br>&nbsp;&nbsp;5.	Membaca isti‟adzah pada rakaat  pertama dan membaca basmalah dengan suara pelan pada tiap-tiap rakaat. Hal ini berdasarkan firman Allah SWT:
+<br>&nbsp;&nbsp;“Maka  apabila  kamu  membaca  Al  Qur‟an,  maka  hendaklah  kamu  memohon  kepada  Allah  dari (godaan) syaitan yang terkutuk”. (QS. An Nahl: 98).
+<br>&nbsp;&nbsp;6.	Membaca “aamiin” setelah membaca surat Al Fatihah. Hal ini disunnahkan kepada setiap orang yang sholat, baik sebagai imam maupun makmum atau sholat sendirian.
+
+      </p>
+      <h2 id="tatacara">gerakan sholat</h2>
+      <div class="row">
+        <div class="col-4">
+          <h5>1. Takbiratul ihram</h5>
+          <img src="<?=base_url('assets/vendor/asset-materi/image/');?>takbir.jpg"  class="rounded img-fluid">
+        </div>
+        <div class="col-4">
+          <h5>2. Rukuk</h5>
+          <img src="<?=base_url('assets/vendor/asset-materi/image/');?>rukuk.jpg"  class="rounded img-fluid">
+        </div>
+        <div class="col-4">
+          <h5>3. I‟tidal</h5>
+          <img src="<?=base_url('assets/vendor/asset-materi/image/');?>iktidal.jpg"  class="rounded img-fluid">
+        </div>
+        <div class="col-4">
+          <h5>4. Sujud</h5>
+          <img src="<?=base_url('assets/vendor/asset-materi/image/');?>sujud.jpg"  class="rounded img-fluid">
+        </div>
+        <div class="col-4">
+          <h5>5. Sujud</h5>
+          <img src="<?=base_url('assets/vendor/asset-materi/image/');?>sujud2.jpg"  class="rounded img-fluid">
+        </div>
+        <div class="col-4">
+          <h5>6. Duduk Diantara 2 Sujud</h5>
+          <img src="<?=base_url('assets/vendor/asset-materi/image/');?>dudukdiantara.jpg"  class="rounded img-fluid">
+        </div>
+        <div class="col-4">
+          <h5>7. Duduk Diantara 2 Sujud</h5>
+          <img src="<?=base_url('assets/vendor/asset-materi/image/');?>dudukdiantara2.jpg"  class="rounded img-fluid">
+        </div>
+        <div class="col-4">
+          <h5>8. Duduk atahiat akhir</h5>
+          <img src="<?=base_url('assets/vendor/asset-materi/image/');?>dudukakhir.jpg"  class="rounded img-fluid">
+        </div>
+        <div class="col-4">
+          <h5>9. Duduk atahiat akhir</h5>
+          <img src="<?=base_url('assets/vendor/asset-materi/image/');?>dudukakhir2.jpg"  class="rounded img-fluid">
+        </div>
+      </div>
+      <p></p>    
     </div>
   </div>
-</div>
