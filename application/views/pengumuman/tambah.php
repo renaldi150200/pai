4f480c41799ee191fa8fbd806ea161f3f8c367b2
Renaldi Panji <renaldipanji150200@gmail.com>
2020-09-07 19:59:48 +0800

    fix form pengumuman

--- application/views/pengumuman/tambah.php
+++ application/views/pengumuman/tambah.php
@@ -64,8 +64,8 @@
                     <label for="exampleFormControlTextarea1">Deksripsi</label>
                     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="deskripsi"></textarea>
                 </div>
+                <button type="submit" class="btn btn-primary">Tambah</button>
             </form>
-            <button type="submit" class="btn btn-primary">Tambah</button>
         </div>
     </div>
 
